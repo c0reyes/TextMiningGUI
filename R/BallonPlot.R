ab4556c69383c2db00dca88ae4a2eb33b71bebc2
Conrado Reyes <coreyes@gmail.com>
2020-04-18 20:18:31 +0200

    Ballon plot.

---
+++ R/BallonPlot.R
@@ -0,0 +1,15 @@
+BallonPlot <- function() {
+    Plot <- function(graph) {
+        colors <- c("#0D0887FF", "#6A00A8FF", "#B12A90FF", "#E16462FF", "#FCA636FF", "#F0F921FF")
+
+        set.seed(0)
+        w <- tm$data[1:50,]
+        pb <- ggballoonplot(w, fill = "value") +
+                scale_fill_gradientn(colors = colors) +
+                guides(size = FALSE)
+                
+        return(pb)
+    }
+
+    PageGUI("Ballon Plot", Plot, 1.5, 1.5)
+}
