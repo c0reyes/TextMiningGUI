bcd1dff59e2e13ce12b51b9f686c78bb8deaf136
Conrado Reyes <coreyes@gmail.com>
2020-04-18 20:18:49 +0200

    Fixed dimention.

--- R/WorldCloud.R
+++ R/WorldCloud.R
@@ -18,5 +18,5 @@ WorldCloud <- function() {
         return(pw)
     }
 
-    PageGUI("World Cloud", Plot, 1.25, 1.25, theme = "theme_void", palette = "Dark2", background = "#ffffff")
+    PageGUI("World Cloud", Plot, 1.5, 1.5, theme = "theme_void", palette = "Dark2", background = "#ffffff")
 }
